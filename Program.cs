b0990f97d9cee8ac87b466ffba7791b473e4dd0e
alexr63 <alex@resnik.co.ua>
2022-08-31 14:42:26 +0300

    remove Orleans

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Orleans.Hosting;
 
 namespace BlazorVanillaServer
 {
@@ -9,13 +8,6 @@ namespace BlazorVanillaServer
         public static void Main(string[] args)
         {
             CreateHostBuilder(args)
-                .UseOrleans(builder =>
-                {
-                    builder.UseLocalhostClustering();
-                    builder.AddMemoryGrainStorageAsDefault();
-                    builder.AddSimpleMessageStreamProvider("SMS");
-                    builder.AddMemoryGrainStorage("PubSubStore");
-                })
                 .Build().Run();
         }
 
